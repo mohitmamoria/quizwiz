c05b6253b60348aa3be2ed4d5f48c75d0f262017
Mohit Mamoria <mohit.mamoria@gmail.com>
2024-04-15 15:54:30 +0530

    made the design for the player

--- resources/js/Pages/Game/Play.vue
+++ resources/js/Pages/Game/Play.vue
@@ -1,5 +1,6 @@
 <script setup>
-import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { useAPIForm } from "@/Composables/useAPIForm";
+import GameplayHeader from "@/Partials/GameplayHeader.vue";
 import { Head } from "@inertiajs/vue3";
 
 const props = defineProps({
@@ -8,14 +9,57 @@ const props = defineProps({
         required: true,
     },
 });
+
+const form = useAPIForm({
+    answer: "",
+});
 </script>
 
 <template>
     <Head title="Enter the arena" />
 
     <div class="py-8 px-8 max-w-xl mx-auto">
-        <ApplicationLogo class="mx-auto mb-16"></ApplicationLogo>
+        <GameplayHeader></GameplayHeader>
 
-        Game loads here.
+        <article class="prose mb-16">
+            <p>An amazing question loads here.</p>
+            <p>
+                Can be multiple lines, with a lot of hints to make it deducable,
+                only if thought carefully.
+            </p>
+            <p>Would you solve it?</p>
+        </article>
+
+        <form class="space-y-2" action="">
+            <div>
+                <label
+                    for="answer"
+                    class="block text-xs font-medium leading-6 text-yellow-600 uppercase"
+                    >What's your answer?</label
+                >
+                <div>
+                    <input
+                        id="answer"
+                        name="answer"
+                        type="text"
+                        required
+                        placeholder="Your attempt..."
+                        autofocus
+                        v-model="form.answer"
+                        class="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-600"
+                    />
+                </div>
+            </div>
+
+            <div>
+                <button
+                    type="submit"
+                    :disabled="form.processing"
+                    class="flex w-full justify-center rounded-md bg-yellow-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
+                >
+                    Submit
+                </button>
+            </div>
+        </form>
     </div>
 </template>
---
+++ resources/js/Partials/GameplayHeader.vue
@@ -0,0 +1,38 @@
+<script setup>
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+</script>
+
+<template>
+    <div class="mb-16">
+        <ApplicationLogo class="mx-auto mb-4"></ApplicationLogo>
+
+        <div class="mx-auto flex items-center justify-center space-x-5">
+            <div class="flex items-end">
+                <img
+                    class="w-6 h-6"
+                    src="/images/icons/icon-health.svg"
+                    alt="Health"
+                />
+                <h4 class="text-[#058F66] font-extrabold">100</h4>
+            </div>
+
+            <div class="flex items-end">
+                <img
+                    class="w-6 h-6"
+                    src="/images/icons/icon-timer.svg"
+                    alt="Timer"
+                />
+                <h4 class="text-[#477ED2] font-extrabold">00:00</h4>
+            </div>
+
+            <div class="flex items-end">
+                <img
+                    class="w-6 h-6"
+                    src="/images/icons/icon-leaderboard.svg"
+                    alt="Leaderboard"
+                />
+                <h4 class="text-[#9B2BB7] font-extrabold">#1</h4>
+            </div>
+        </div>
+    </div>
+</template>
