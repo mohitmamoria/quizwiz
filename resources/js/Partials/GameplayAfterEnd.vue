9b9470f5b64372f6d8e49ea1681c5355b57c992e
Mohit Mamoria <mohit.mamoria@gmail.com>
2024-07-02 16:14:15 +0530

    updated the end screen

--- resources/js/Partials/GameplayAfterEnd.vue
+++ resources/js/Partials/GameplayAfterEnd.vue
@@ -19,24 +19,27 @@ const props = defineProps({
 
 <template>
     <article class="prose mb-16 text-center">
-        <p>Thank you for playing today.</p>
+        <p>Thank you for playing today. Did you enjoy?</p>
     </article>
 
     <div v-if="game.is_solo">
         <div class="bg-white rounded p-4 border border-yellow-400">
-            <h1 class="uppercase text-xs font-thin">RESULT</h1>
-            <p class="mt-4 text-xl font-bold">{{ discountWon.reason }}</p>
+            <p class="mt-4 text-lg font-bold">{{ discountWon.reason }}</p>
             <p class="mt-4">
-                Use coupon code:
-                <span class="font-bold">{{ discountWon.discount_code }}</span>
-                to redeem your discount (valid only for the next 30 minutes) to
-                our next online show
-                <a
-                    class="font-medium underline text-blue-500"
-                    :href="discountWon.discounted_link"
-                    >on this link.</a
-                >
+                Enjoyed playing this quiz? You will love our 90-min online quiz
+                show full of fun and learning! Also, make new friends.
             </p>
+            <p class="mt-4">
+                Use your winning coupon code:
+                <span class="font-bold">{{ discountWon.discount_code }}</span>
+                to redeem your discount (valid only for the next 30 minutes).
+            </p>
+            <a
+                :href="discountWon.discounted_link"
+                target="_blank"
+                class="mt-4 flex w-full justify-center rounded-md bg-yellow-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600 disabled:opacity-50"
+                >Know more about the online quiz show &rarr;</a
+            >
         </div>
     </div>
 
